03c8e6fc05dafd93673314b9baa76e8e6b9fa2e4
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2026-04-04 18:13:14 +0200

    add snack.explorer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,8 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-
 ---@module 'lazy'
 ---@type LazySpec
 
@@ -17,6 +12,7 @@ return {
     lazy = false,
     keys = {
       { '<leader>dd', function() Snacks.dashboard() end, desc = 'Dashboard' },
+      { '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
     },
     opts = {
       dashboard = {
