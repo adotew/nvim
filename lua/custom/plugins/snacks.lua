9c26fcf5d878a17f55807c0ab193f1147e28cf4b
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2026-04-05 11:28:00 +0200

    split custom plugins in seperate files up

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,39 @@
+---@module 'lazy'
+---@type LazySpec
+
+return {
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    keys = {
+      { '<leader>dd', function() Snacks.dashboard() end, desc = 'Dashboard' },
+      { '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
+      { '<leader>gg', function() Snacks.lazygit() end, desc = 'LazyGit' },
+    },
+    opts = {
+      lazygit = {},
+      dashboard = {
+        pane_gap = 12,
+        sections = {
+          { section = 'keys', gap = 1, padding = 1 },
+          { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = ' ',
+            title = 'Git Status',
+            section = 'terminal',
+            enabled = function() return Snacks.git.get_root() ~= nil end,
+            cmd = 'git status --short --branch --renames',
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = 'startup' },
+        },
+      },
+    },
+  },
+}
