31c44eb7594a6d284bdc4953a0033bb187dbdce0
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2026-04-06 18:52:24 +0200

    add treesitter-textobject plugin

---
+++ lua/custom/plugins/treesitter-textobjects.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter-textobjects').setup {
+        select = {
+          lookahead = true,
+          keymaps = {
+            ['af'] = { query = '@function.outer', desc = 'Around function' },
+            ['if'] = { query = '@function.inner', desc = 'Inside function' },
+            ['ac'] = { query = '@class.outer', desc = 'Around class' },
+            ['ic'] = { query = '@class.inner', desc = 'Inside class' },
+            ['aa'] = { query = '@parameter.outer', desc = 'Around argument' },
+            ['ia'] = { query = '@parameter.inner', desc = 'Inside argument' },
+          },
+        },
+        move = {
+          set_jumps = true,
+          goto_next_start = {
+            [']m'] = { query = '@function.outer', desc = 'Next function start' },
+            [']]'] = { query = '@class.outer', desc = 'Next class start' },
+          },
+          goto_next_end = {
+            [']M'] = { query = '@function.outer', desc = 'Next function end' },
+            [']['] = { query = '@class.outer', desc = 'Next class end' },
+          },
+          goto_previous_start = {
+            ['[m'] = { query = '@function.outer', desc = 'Previous function start' },
+            ['[['] = { query = '@class.outer', desc = 'Previous class start' },
+          },
+          goto_previous_end = {
+            ['[M'] = { query = '@function.outer', desc = 'Previous function end' },
+            ['[]'] = { query = '@class.outer', desc = 'Previous class end' },
+          },
+        },
+        swap = {
+          swap_next = {
+            ['<leader>a'] = { query = '@parameter.inner', desc = 'Swap with next parameter' },
+          },
+          swap_previous = {
+            ['<leader>A'] = { query = '@parameter.inner', desc = 'Swap with previous parameter' },
+          },
+        },
+      }
+    end,
+  },
+}
